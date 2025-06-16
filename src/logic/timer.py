e20fce556d77dc1ee435644ecadce1a37ada5694
Isaiah Jacobsen <isaiah.jacobsen1@gmail.com>
2025-06-15 21:24:47 -0500

    Added timer class

---
+++ src/logic/timer.py
@@ -0,0 +1,31 @@
+import time
+
+
+class Timer:
+    def __init__(self, scheduled_start_time: float, game_length: float = 7200):
+        self._start_time = scheduled_start_time
+        self._game_end_time = self._start_time + game_length
+
+    def is_pre_game(self) -> bool:
+        return time.time() < self._start_time
+
+    def get_pre_game_time(self) -> float:
+        return max(0.0, self._start_time - time.time())
+
+    def is_game_over(self) -> bool:
+        return time.time() > self._game_end_time
+
+    def get_remaining_game_time(self) -> float:
+        if self.is_pre_game():
+            return 0.0
+        return max(0.0, self._game_end_time - time.time())
+
+    def get_elapsed_game_time(self) -> float:
+        if self.is_pre_game():
+            return 0.0
+        return min(time.time() - self._start_time, self._game_end_time - self._start_time)
+
+
+if __name__ == '__main__':
+    pass
+
